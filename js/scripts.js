eb9ed4c763fe711e4df5760e5c90964cac0a3dac
Justin Kerntz and Jaime Takikawa <justin@>
2019-04-04 14:14:02 -0700

    add logic to roman numeral outputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 //* front end logic *//
 $(document).ready(function(){
+  var romanNumeralOutput = [];
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("input#userNumber").val());
-    romanNumeralConverter(userInput);
+    var userInput = $("input#userNumber").val();
+    var arrayNumerals = userInput.split("");
+    console.log(arrayNumerals);
+    romanNumerals(userInput);
+    romanNumeralConverter(romanNumerals);
   })
 
 
 
 //* back end logic *//
-  function romanNumeralConverter(userInput){
-    var romanNumeralOutput = [];
+  function romanNumerals(userInput){
     // if(userInput === 1){
     //   romanNumeralOutput.push("I");
     // }else if (userInput < 4) {
@@ -29,10 +32,20 @@ $(document).ready(function(){
         userInput = i;
         console.log(romanNumeralOutput);
       }else if (userInput >= 500){
-        romanNumeralOutput.push("D");
-        var i = (i - 500);
-        userInput = i;
+        if(userInput >= 900){
+          romanNumeralOutput.push("CM");
+          var i = (i - 900);
+          userInput = i;
+        }else{
+          romanNumeralOutput.push("D");
+          var i = (i - 500);
+          userInput = i
+        }
       }else if (userInput >= 100){
+        if(userInput >= 400){
+          romanNumeralOutput.push("CD");
+          userInput = i;
+        }else{}
         romanNumeralOutput.push("C");
         var i = (i - 100);
         userInput = i;
@@ -42,12 +55,21 @@ $(document).ready(function(){
         userInput = i;
       }else if (userInput >= 10){
         romanNumeralOutput.push("X");
+        var i = (i - 10);
         userInput = i;
       }else if (userInput >= 5){
         romanNumeralOutput.push("V");
+        var i = (i - 5);
+        userInput = i;
+      }else{
+        romanNumeralOutput.push("I")
+        var i = (i - 1)
         userInput = i;
       }
       console.log(romanNumeralOutput);
     }
   }
+
+  function romanNumeralConverter(){}
+
 });
