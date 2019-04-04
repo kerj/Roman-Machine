5fb158b781434b8ea11c38cd83959026225e804c
Justin Kerntz and Jaime Takikawa <justin@>
2019-04-04 14:43:00 -0700

    add finished logic for roman numeral output

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,6 @@ $(document).ready(function(){
         romanNumeralOutput.push("M");
         var i =  (i - 1000);
         userInput = i;
-        console.log(romanNumeralOutput);
       }else if (userInput >= 500){
         if(userInput >= 900){
           romanNumeralOutput.push("CM");
@@ -39,37 +38,61 @@ $(document).ready(function(){
         }else{
           romanNumeralOutput.push("D");
           var i = (i - 500);
-          userInput = i
+          userInput = i;
         }
       }else if (userInput >= 100){
         if(userInput >= 400){
           romanNumeralOutput.push("CD");
+          var i = (i - 400)
           userInput = i;
-        }else{}
-        romanNumeralOutput.push("C");
-        var i = (i - 100);
-        userInput = i;
+        }else{
+          romanNumeralOutput.push("C");
+          var i = (i - 100);
+          userInput = i;
+        }
       }else if (userInput >= 50){
-        romanNumeralOutput.push("L");
-        var i = (i - 50);
-        userInput = i;
+        if(userInput >= 90){
+          romanNumeralOutput.push("XC");
+          var i = (i - 90)
+          userInput = i;
+        }else{
+          romanNumeralOutput.push("L");
+          var i = (i - 50);
+          userInput = i;
+        }
       }else if (userInput >= 10){
-        romanNumeralOutput.push("X");
-        var i = (i - 10);
-        userInput = i;
+        if (userInput >= 40){
+          romanNumeralOutput.push("XL")
+          var i = (i - 40)
+          userInput = i;
+        }else{
+          romanNumeralOutput.push("X");
+          var i = (i - 10);
+          userInput = i;
+        }
       }else if (userInput >= 5){
-        romanNumeralOutput.push("V");
-        var i = (i - 5);
-        userInput = i;
-      }else{
-        romanNumeralOutput.push("I")
-        var i = (i - 1)
-        userInput = i;
+        if (userInput >= 9){
+          romanNumeralOutput.push("IX")
+          var i = (i - 9)
+          userInput = i;
+        }else{
+          romanNumeralOutput.push("V");
+          var i = (i - 5);
+          userInput = i;
+        }
+      }else if (userInput >= 4){
+          romanNumeralOutput.push("IV")
+          var i = (i - 4)
+          userInput = i;
+        }else{
+          romanNumeralOutput.push("I")
+          var i = (i - 1)
+          userInput = i;
+        }
+        console.log(romanNumeralOutput);
       }
-      console.log(romanNumeralOutput);
     }
-  }
 
-  function romanNumeralConverter(){}
+    function romanNumeralConverter(){}
 
-});
+  });
